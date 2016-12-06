d3bc2f083412f1cea692adf030d4ca4e52d32e7c
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-06 15:29:59 -0600

    added basic math functions to js file

---
+++ calculator.js
@@ -0,0 +1,47 @@
+
+//   Create a function that multiplies two numbers
+//   passed in as arguments. Return the product.
+
+var mult = function (numb1, numb2) {
+  var answer = numb1 * numb2;
+  return answer;
+}
+
+
+// //   Create a function that adds two numbers
+//   passed in as arguments. Return the sum.
+
+var add = function (numb1, numb2) {
+  var answer = numb1 + numb2;
+  return answer;
+}
+
+
+
+//   Create a function that subtracts two numbers
+//   passed in as arguments. Return the difference.
+
+var sub = function (numb1, numb2) {
+  var answer = numb1 - numb2;
+  return answer;
+}
+
+
+
+
+//   Create a function that divides two numbers
+//   passed in as arguments. Return the quotient.
+var sub = function (numb1, numb2) {
+  var answer = numb1 / numb2;
+  return answer;
+}
+
+
+
+
+//   Create a function that accepts three arguments.
+//     1. First number
+//     2. Second number
+//     3. A function that performs an operation on them
+
+//   Return the value of the operation.
