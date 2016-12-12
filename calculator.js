db7c69cbc3b332c6a85b6fc525ad3574fa6bd8b5
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-12 14:19:52 -0600

    added functionality to the event listeners

--- calculator.js
+++ calculator.js
@@ -43,27 +43,32 @@ var dvid = function (numb1, numb2) {
 //     1. First number
 //     2. Second number
 //     3. A function that performs an operation on them
-var getNumb = function() {
-  //gather numbers from input fields
-  var num1 =  document.querySelector("#numb1").value;
-  var num2 =  document.querySelector("#numb2").value;
+var getNumb = function(e, opp) {
+  e.preventDefault();
+  //gather numbers from input fields and converts them into integers from strings
+  var num1 =  parseInt(document.querySelector("#numb1").value);
+  var num2 =  parseInt(document.querySelector("#numb2").value);
   //   Return the value of the operation.
-  var answer =  opp(numb1, numb2);
-//Put answer in html page
-document.querySelector("#answer").innerHTML = answer;
+  doMath(num1, num2, opp);
+
+}
+
+var doMath = function (num1, num2, opp) {
+  var answer = opp(num1, num2);
+  console.log(answer);
+  //Put answer in html page
+  document.querySelector("#answer").innerHTML = answer;
 }
 
 
-
-
 //assigning variables to buttons on the DOM
-var addBut = document.querySelector("#add").value;
-var subBut = document.querySelector("#sub").value;
-var divBut = document.querySelector("#div").value;
-var multBut = document.querySelector("#mult").value;
+var addBut = document.querySelector("#add");
+var subBut = document.querySelector("#sub");
+var divBut = document.querySelector("#div");
+var multBut = document.querySelector("#mult");
 
-//assigning event listeners to each button
-// addBut.addEventListener("click", adding);
-subBut.addEventListener("click", sub);
-divBut.addEventListener("click", dvid);
-multBut.addEventListener("click", mult);
+//assigning event listeners to each button, prevents the default reloading of the page and passes the math operation called by that particular button
+subBut.addEventListener("click", function (e) {e.preventDefault(); getNumb(e, sub) });
+divBut.addEventListener("click", function (e) { e.preventDefault(); getNumb(e, dvid) });
+multBut.addEventListener("click", function (e) { e.preventDefault(); getNumb(e, mult) });
+addBut.addEventListener("click", function (e) { e.preventDefault(); getNumb(e, adding) });
